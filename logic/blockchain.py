a1798f4021a34652a215edf1ddc4a992d52ded5e
willowsenator <willowsenator@gmail.com>
2022-07-12 22:54:24 +0200

    proof of work POW

--- logic/blockchain.py
+++ logic/blockchain.py
@@ -20,7 +20,7 @@ class BlockChain:
         """
             Create a new block
 
-        :param proof: hash current block
+        :param proof: nonce current block
         :param previous_hash: hash previous block
         :return: new block created
         """
@@ -31,4 +31,33 @@ class BlockChain:
             'previous_hash': previous_hash
         }
 
+        self.chain.append(block)
         return block
+
+    def get_last_block(self):
+        """
+            Get last block of blockchain
+
+        :return: last block of blockchain
+        """
+
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        """
+            POW(Proof of works consensus protocol)
+
+        :param previous_proof: previous nonce
+        :return: new nonce
+        """
+
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+
+        return new_proof
